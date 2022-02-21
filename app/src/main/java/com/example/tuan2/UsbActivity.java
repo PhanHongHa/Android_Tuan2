1eb6a162ae3e2ce9a88ef7c96da12f34545961e6
Student <phanhongha96@gmail.com>
2022-02-21 14:25:24 +0700

    ui2

---
+++ app/src/main/java/com/example/tuan2/UsbActivity.java
@@ -0,0 +1,14 @@
+package com.example.tuan2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UsbActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.usb);
+    }
+}
